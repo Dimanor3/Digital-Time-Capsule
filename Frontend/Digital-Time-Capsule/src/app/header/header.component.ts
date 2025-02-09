2bbcca43fef447d8aec19b0602382e024aa18908
root <root@bijanrazavi.com>
2025-02-09 10:02:33 +0000

    Header Version 1

--- Frontend/Digital-Time-Capsule/src/app/header/header.component.ts
+++ Frontend/Digital-Time-Capsule/src/app/header/header.component.ts
@@ -1,12 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'header-root',
     imports: [RouterLink],
     templateUrl: './header.component.html',
-    styleUrl: './header.component.css'
+    styleUrl: './header.component.css',
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HeaderComponent {
-    
+    collapsed = true;
 }
